b6e00a96e2619cbaec8e78593ebc45f088dd8baa
Louis-Olivier Brassard <louis@loupbrun.ca>
2021-04-29 19:04:53 -0400

    config staging

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 USER=louis
-SERVERNAME=nouveau.demimarathondeblainville.com
-REMOTEDIR=/var/www/nouveau.demimarathondeblainville.com/public/
+SERVERNAME=www-test.demimarathondeblainville.com
+REMOTEDIR=/var/www/www-test.demimarathondeblainville.com/public/
 
 rsync -vzr --exclude-from="exclude.deploy" --delete public/ $USER@$SERVERNAME:$REMOTEDIR
